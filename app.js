c9dc96f20284e852aca6965294efb4996f484147
mohammadxxali <ali_mohammad@t-online.de>
2023-04-27 18:54:07 +0200

    add app js edits to clearBtn

--- app.js
+++ app.js
@@ -16,3 +16,8 @@ clrs.forEach(clr => {
         ctx.strokeStyle = clr.dataset.clr;
     })
 })
+
+let clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
